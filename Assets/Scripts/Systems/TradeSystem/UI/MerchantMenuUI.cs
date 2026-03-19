36bf30ddccee0313f7b0a4de9a3feb9750a14a82
W0odz <pedrovmadeira@gmail.com>
2026-03-19 16:13:51 -0300

    Implementação da Quest 2 (7)

--- Assets/Scripts/Systems/TradeSystem/UI/MerchantMenuUI.cs
+++ Assets/Scripts/Systems/TradeSystem/UI/MerchantMenuUI.cs
@@ -116,11 +116,10 @@ public class MerchantMenuUI : MonoBehaviour
         foreach (var slot in InventoryManager.Instance.listaItens)
         {
             if (slot.item == null) continue;
-<<<<<<< Updated upstream
             DadosItem itemCapturado = slot.item;
-=======
+
             if (slot.item.naoVendivel) continue;
->>>>>>> Stashed changes
+
             var go = Instantiate(itemSlotPrefab, inventarioContent);
             go.GetComponent<MerchantItemSlotUI>().Setup(itemCapturado, () => AdicionarBarraJogador(itemCapturado, 1));
         }
@@ -240,10 +239,9 @@ public class MerchantMenuUI : MonoBehaviour
         foreach (var slot in InventoryManager.Instance.listaItens)
         {
             if (slot.item == null) continue;
-<<<<<<< Updated upstream
-=======
+
             if (slot.item.naoVendivel) continue;
->>>>>>> Stashed changes
+
 
             int naFarra = 0;
             foreach (var (barItem, barQty) in _barraJogador)
